e7d97fbb968de9e41d90e92961e34fa9154ef557
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2024-12-01 16:11:16 +0100

    Suppression

--- jeux/Jeux_3D/Zelda3D_KOMODZINSKI/godot-botw - start/scenes/entities/other/bird.gd
+++
@@ -1,5 +0,0 @@
-extends PathFollow3D
-
-
-func _process(delta: float) -> void:
-	progress += 40 * delta
--- jeux/Jeux_3D/Zelda3D_KOMODZINSKI/godot-botw - start/scenes/levels/overworld.gd
+++
@@ -1,6 +0,0 @@
-extends Level
-
-
-
-func _on_castle_area_body_entered(_body: Node3D) -> void:
-	switch_level('dungeon')

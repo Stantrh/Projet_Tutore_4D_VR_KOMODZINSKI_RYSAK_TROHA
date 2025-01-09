43e90456bd85d554050a72c77c74933573fe3284
Hugo Rysak <hugo.rysak4@etu.univ-lorraine.fr>
2025-01-09 08:29:57 +0100

    Bug fix

--- app4d/Scripts/ui.gd
+++ app4d/Scripts/ui.gd
@@ -2,14 +2,7 @@ extends Control
 
 @export var object_controlled : Node
 @onready var translation_sliders : Array = [$ReferenceRect/ColorRect/Translation/HBoxContainer/translation_x_slider,$ReferenceRect/ColorRect/Translation/HBoxContainer2/translation_y_slider,$ReferenceRect/ColorRect/Translation/HBoxContainer3/translation_z_slider,$ReferenceRect/ColorRect/Translation/HBoxContainer4/translation_w_slider]
-@onready var rotation_sliders : Array = [
-	$ReferenceRect/ColorRect/Rotation/HBoxContainer/VBoxContainer/HBoxContainer5/HBoxContainer/xy_rotation_slider,
-	$ReferenceRect/ColorRect/Rotation/HBoxContainer/VBoxContainer/HBoxContainer6/HBoxContainer2/xz_rotation_slider,
-	$ReferenceRect/ColorRect/Rotation/HBoxContainer/VBoxContainer/HBoxContainer7/HBoxContainer3/yz_rotation_slider,
-	$ReferenceRect/ColorRect/Rotation/HBoxContainer/VBoxContainer2/HBoxContainer5/HBoxContainer4/xw_rotation_slider,
-	$ReferenceRect/ColorRect/Rotation/HBoxContainer/VBoxContainer2/HBoxContainer6/HBoxContainer5/yw_rotation_slider,
-	$ReferenceRect/ColorRect/Rotation/HBoxContainer/VBoxContainer2/HBoxContainer7/HBoxContainer6/zw_rotation_slider
-]
+
 
 # Pour les sliders, on garde les positions des sliders précédents dans un dictionnaire qu'on actualise
 var prev_translation_pos = {} # translations
